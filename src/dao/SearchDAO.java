0409f31e3cb01c53b41b25aef1820e772878046e
plusdojo2024-tanaka <tanaka-atsushi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 15:33:35 +0900

    たけのこ

--- src/dao/SearchDAO.java
+++ src/dao/SearchDAO.java
@@ -3,13 +3,12 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 //userテーブルとやり取りをするクラス
-public class ListReviewsDAO {
+public class SearchDAO {
 
-	public int insert(int listId, int reviewId) {
+	public int insert(String backnumberContent) {
 		int num = 0;
 		Connection conn = null;
 
@@ -24,12 +23,9 @@ public class ListReviewsDAO {
 			conn=DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A4DB",id,pw);
 
 			// SELECT文を準備する
-			String sql = "INSERT INTO list_reviews (list_id, review_id) VALUES (?, ?)";
+			String sql = "INSERT INTO backnumbers VALUES (NULL, ReviewsDAO.selectId(), ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			// SQL文を完成させる
-			pStmt.setInt(1, listId);//引数sqlにsetStringしてる
-			pStmt.setInt(2, reviewId);
+			pStmt.setString(1, backnumberContent);//引数sqlにsetStringしてる
 
 			// SQL文を実行し、結果表を取得する	検索して結果の表をrsに入れる構文
 			num = pStmt.executeUpdate();
@@ -56,7 +52,7 @@ public class ListReviewsDAO {
 		return num;
 	}
 
-	public int delete(int listReviewId) {
+	public int delete(int backnumberId) {
 		int num = 0;
 		Connection conn = null;
 
@@ -71,11 +67,11 @@ public class ListReviewsDAO {
 			conn=DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A4DB",id,pw);
 
 			// SELECT文を準備する
-			String sql = "DELETE FROM WHERE list_review_id=?";
+			String sql = "DELETE FROM backnumbers WHERE backnumberId = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			pStmt.setInt(1, listReviewId);
+			pStmt.setInt(1, backnumberId);
 
 			// SQL文を実行し、結果表を取得する	検索して結果の表をrsに入れる構文
 			num = pStmt.executeUpdate();
@@ -102,29 +98,30 @@ public class ListReviewsDAO {
 		return num;
 	}
 
-	public int countList(int listId) {
-		Connection conn = null;
+	public int update(int backnumberId, String backnumberContent) {
 		int num = 0;
+		Connection conn = null;
 
 		try {
 			// JDBCドライバを読み込む
 			Class.forName("org.h2.Driver");
 
+			String id="sa";
+			String pw="";
+
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+			conn=DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/A4DB",id,pw);
 
 			// SELECT文を準備する
-			String sql = "SELECT COUNT(*) list_review_id FROM list_reviews WHERE list_id = ?";
+			String sql = "UPDATE backnumbers SET backnumber_content = ? WHERE backnumber_id = ? ";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			pStmt.setInt(1, listId);
 
-			// SELECT文を実行し、結果表を取得する
-			ResultSet rs = pStmt.executeQuery();
+			// SQL文を完成させる
+			pStmt.setString(1, backnumberContent);//引数sqlにsetStringしてる
+			pStmt.setInt(2, backnumberId);//引数sqlにsetStringしてる
 
-			// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
-			rs.next();
-			num = rs.getInt("COUNT(*)");
-
+			// SQL文を実行し、結果表を取得する	検索して結果の表をrsに入れる構文
+			num = pStmt.executeUpdate();
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -147,4 +144,5 @@ public class ListReviewsDAO {
 		// 結果を返す
 		return num;
 	}
+
 }
