376935414b6ece29f2d78e188dc405f929b4d48b
plusdojo2024-tanaka <tanaka-atsushi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-21 10:58:01 +0900

    たけのこ！

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -32,7 +32,7 @@ public class SearchServlet extends HttpServlet {
 			return;
 		}
 
-		
+
 		// 検索ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search.jsp");
 		dispatcher.forward(request, response);
@@ -42,52 +42,52 @@ public class SearchServlet extends HttpServlet {
 	//ここからdoPost。ここに追加していく。
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-			if (session.getAttribute("id") == null) {
-			response.sendRedirect("/A4/LoginServlet");
-			return;
-		}
-			
-		
+//		HttpSession session = request.getSession();
+//			if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/A4/LoginServlet");
+//			return;
+//		}
+
+
 		//文字化け防止処理構文を記述
 		request.setCharacterEncoding("UTF-8");
-		
+
 		//価格検索のテキストの内容を取得する文
 		String search = request.getParameter("search");
 		String stpriceA = request.getParameter("price_a");
 		int priceA = Integer.parseInt(stpriceA);
 		String stpriceB = request.getParameter("price_b");
 		int priceB = Integer.parseInt(stpriceB);
-		
+
 		//評価検索のテキストの内容を取得する文
 		String stevaA = request.getParameter("eva_a");
 		int evaA = Integer.parseInt(stevaA);
 		String stevaB = request.getParameter("eva_b");
 		int evaB = Integer.parseInt(stevaB);
-		
+
 		//日付検索のテキストの内容を取得する文
 		String stcreateA = request.getParameter("create_a");
 		int createA = Integer.parseInt(stcreateA);
 		String stcreateB = request.getParameter("create_b");
 		int createB = Integer.parseInt(stcreateB);
-		
+
 		//リクエストのアトリビュート区画にセットする文
 		ReviewsDAO dao = new ReviewsDAO();
 		ArrayList<Review> list = dao.select(priceA,priceB,evaA,evaB,createA,createB);
 		request.setAttribute("allSearchList", list);
-		
-		
+
+
 		//Category1の内容を取得
 		Categorys1DAO ca1Dao = new Categorys1DAO();
 		List<Category> ca1List = ca1Dao.select();
-		
+
 		//Category2の内容を取得
 		Categorys2DAO ca2Dao = new Categorys2DAO();
 		List<Category> ca2List = ca2Dao.select();
-		
+
 		// 検索結果をリクエストスコープに格納する
 		request.setAttribute("cardList", cardList);
-		
+
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search.jsp");
 		dispatcher.forward(request, response);
